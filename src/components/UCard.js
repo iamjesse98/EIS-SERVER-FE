5e73aca7fb0e9ce1e497eafd67fbf8ce73ab4763
iamjesse98 <yadagallajaswanthkumar98@gmail.com>
2018-04-05 21:48:14 +0530

    sec

--- src/components/UCard.js
+++ src/components/UCard.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Card, CardActions, CardMedia, CardTitle, CardText } from 'material-ui/Card'
+import { Card, CardActions, CardMedia, CardText } from 'material-ui/Card' // CardTitle
 import FlatButton from 'material-ui/FlatButton'
 import Toggle from 'material-ui/Toggle'
 import '../css/ucard.css'
@@ -7,12 +7,13 @@ import '../css/ucard.css'
 // let expanded = false
 // let tog = _ => {expanded = !expanded; console.log(expanded)}
 
+/* overlay={<CardTitle title={props.ttl} subtitle={props.sub} />} */
+
 export default class UCard extends Component {
     render() {
         let props = this.props
         return (<Card>
             <CardMedia
-                overlay={<CardTitle title={props.ttl} subtitle={props.sub} />}
             >
                 <img src={props.sce} alt={props.ate} />
             </CardMedia>
